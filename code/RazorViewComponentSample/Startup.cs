af7785b8abf97d02990987334663966bb5b8c687
Kip <kstreith@yahoo.com>
2019-08-04 21:58:34 -0400

    Remove commented out code

--- code/RazorViewComponentSample/Startup.cs
+++ code/RazorViewComponentSample/Startup.cs
@@ -1,13 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using System;
-using System.Linq;
-using System.Reflection;
 
 namespace RazorViewComponentSample
 {
@@ -24,42 +20,11 @@ namespace RazorViewComponentSample
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             var mvc = services.AddMvc();
-            //var assembly = typeof(Startup).GetTypeInfo().Assembly;
-            //mvc.AddApplicationPart(assembly);
             ConfigureMvc(mvc);
-            //AddControllersAsServices2(mvc);
             mvc.SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             return services.BuildServiceProvider();
         }
-
-        //public class ServiceBasedControllerActivator2 : IControllerActivator
-        //{
-        //    public object Create(ControllerContext actionContext)
-        //    {
-        //        var controllerType = actionContext.ActionDescriptor.ControllerTypeInfo.AsType();
-
-        //        return actionContext.HttpContext.RequestServices.GetRequiredService(controllerType);
-        //    }
-
-        //    public virtual void Release(ControllerContext context, object controller)
-        //    {
-        //    }
-        //}
-        //public static IMvcBuilder AddControllersAsServices2(IMvcBuilder builder)
-        //{
-        //    var feature = new ControllerFeature();
-        //    builder.PartManager.PopulateFeature(feature);
-
-        //    foreach (var controller in feature.Controllers.Select(c => c.AsType()))
-        //    {
-        //        builder.Services.TryAddTransient(controller, controller);
-        //    }
-
-        //    builder.Services.Replace(ServiceDescriptor.Transient<IControllerActivator, ServiceBasedControllerActivator2>());
-
-        //    return builder;
-        //}
 
         public virtual void ConfigureMvc(IMvcBuilder mvc)
         {
--- code/RazoreViewComponentSample.Tests/TestWebServerFixture.cs
+++ code/RazoreViewComponentSample.Tests/TestWebServerFixture.cs
@@ -1,13 +1,8 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 
 namespace RazorViewComponentSample.Tests
 {
